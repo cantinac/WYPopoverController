561e76a8f133f2a0bc65039e3b8de04fcd01bfdd
Armando Di Cianno <armando@dicianno.org>
2016-02-12 15:08:59 -0500

    Add new framework projet as Shared to new workspace

---
+++ frameworks/WYPopover/WYPopover/WYPopover.h
@@ -0,0 +1,21 @@
+//
+//  WYPopover.h
+//  WYPopover
+//
+//  Created by Armando Di Cianno on 2/12/16.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for WYPopover.
+FOUNDATION_EXPORT double WYPopoverVersionNumber;
+
+//! Project version string for WYPopover.
+FOUNDATION_EXPORT const unsigned char WYPopoverVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WYPopover/PublicHeader.h>
+
+
+#import <WYPopover/WYPopoverController.h>
+#import <WYPopover/WYStoryboardPopoverSegue.h>
